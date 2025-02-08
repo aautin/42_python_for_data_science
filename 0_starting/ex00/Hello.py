07240bbd9fd96b21c2ecc44f4b69645a0380a6b8
aautin <ezagof@gmail.com>
2025-02-08 17:53:30 +0100

    ex00 not executing (tuple dict array and list diffs?)

---
+++ 0_starting/ex00/Hello.py
@@ -0,0 +1,16 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#your code here
+ft_list[1] = "World!"
+ft_tuple[1] = "France!"
+ft_set[1] = "Paris!"
+ft_dict[1] = "42Paris!"
+#--
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
