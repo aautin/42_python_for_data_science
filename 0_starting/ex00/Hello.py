58372d5ba21f610445c83452fe3ce827cb0d10e4
aautin.home <alexandreautin.aa@gmail.com>
2025-02-09 20:16:31 +0100

    fix ft_set access

--- 0_starting/ex00/Hello.py
+++ 0_starting/ex00/Hello.py
@@ -1,13 +1,17 @@
-ft_list = ["Hello", "tata!"]
-ft_tuple = ("Hello", "toto!")
-ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
+ft_list = ["Hello", "tata!"] #dynamic arrays
+ft_tuple = ("Hello", "toto!") #fixed memory block stored on program's stack
+ft_set = {"Hello", "tutu!"} #hash table (content is hashed to definite each key)
+ft_dict = {"Hello" : "titi!"} #hash table with personalized keys to order content
 
 #your code here
 ft_list[1] = "World!"
-ft_tuple[1] = "France!"
-ft_set[1] = "Paris!"
-ft_dict[1] = "42Paris!"
+
+ft_tuple = ("Hello", "France!")
+
+ft_set.remove("tutu!")
+ft_set.add("Paris!")
+
+ft_dict["Hello"] = "42Paris!"
 #--
 
 print(ft_list)
