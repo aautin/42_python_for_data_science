b4c535d352d7b8e80f43e9a783923e4980608857
aautin.home <alexandreautin.aa@gmail.com>
2025-02-09 21:46:34 +0100

    put code into main function

--- 0_starting/ex00/Hello.py
+++ 0_starting/ex00/Hello.py
@@ -1,20 +1,23 @@
-ft_list = ["Hello", "tata!"] #dynamic arrays
-ft_tuple = ("Hello", "toto!") #fixed memory block stored on program's stack
-ft_set = {"Hello", "tutu!"} #hash table (content is hashed to definite each key)
-ft_dict = {"Hello" : "titi!"} #hash table with personalized keys to order content
+def main():
+    ft_list = ["Hello", "tata!"] #dynamic arrays
+    ft_tuple = ("Hello", "toto!") #fixed memory block stored on program's stack
+    ft_set = {"Hello", "tutu!"} #hash table (content is hashed to definite each key)
+    ft_dict = {"Hello" : "titi!"} #hash table with personalized keys to order content
 
-#your code here
-ft_list[1] = "World!"
+    #your code here
+    ft_list[1] = "World!"
 
-ft_tuple = ("Hello", "France!")
+    ft_tuple = ("Hello", "France!")
 
-ft_set.remove("tutu!")
-ft_set.add("Paris!")
+    ft_set.remove("tutu!")
+    ft_set.add("Paris!")
 
-ft_dict["Hello"] = "42Paris!"
-#--
+    ft_dict["Hello"] = "42Paris!"
+    #--
 
-print(ft_list)
-print(ft_tuple)
-print(ft_set)
-print(ft_dict)
+    print(ft_list)
+    print(ft_tuple)
+    print(ft_set)
+    print(ft_dict)
+
+main()
