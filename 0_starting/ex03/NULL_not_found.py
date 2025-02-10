1c0acf5ef417ee67c2dfc13d5b527c6e06fd74b7
aautin <ezagof@gmail.com>
2025-02-10 15:01:53 +0100

    detect NULL, empty, 0 and false values

--- 0_starting/ex03/NULL_not_found.py
+++ 0_starting/ex03/NULL_not_found.py
@@ -1,3 +1,15 @@
 def NULL_not_found(object: any) -> int:
-    #my code here
-    return 1
+    if type(object) is type(None):
+        print("Nothing: None", type(object))
+    elif type(object) is float and str(object) == "nan":
+        print("Cheese: nan", type(object))
+    elif type(object) is int and object == 0:
+        print("Zero: 0", type(object))
+    elif type(object) is str and object == "":
+        print("Empty: ", type(object))
+    elif type(object) is bool and object == False:
+        print("Fake: False", type(object))
+    else:
+        print("Type not Found")
+        return 1
+    return 0
